32b3ab6ce217fefe96373fda15bc1235bcab13a9
Andres Tafur <luis.tafur@aguayo.co>
2023-03-02 08:10:03 -0500

    Ajustes varios

--- src/app/car/car.page.ts
+++ src/app/car/car.page.ts
@@ -1,36 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController, NavController } from '@ionic/angular';
-import { HeaderBackComponent } from '../components/layout/header-back/header-back.component';
 
 import { LoginService } from '../services/login.service';
 import { ShopingCarService } from '../services/shoping-car.service';
+
+import { HeaderBackComponent } from '../components/layout/header-back/header-back.component';
+
+
 @Component({
   selector: 'app-car',
   templateUrl: './car.page.html',
   providers: [HeaderBackComponent]
 })
+
+
 export class CarPage implements OnInit {
 
   public arrayProducts = new Array()
   public arrayCashback = new Array()
-  isRemoved = false;
-  isCashbackApply = false
-  isSellerApply = false
-  public totalProductPrice: number = 0
-  public subtotalProductPrice: number = 0
+  public isRemoved = false;
+  public isCashbackApply = false
+  public isSellerApply = false
+  public subtotalProductPrice: any = 0
   public totalProductPriceProcess: any = 0
-  public isTotalCompra = false
   public totalCashback: any = 0
   public cashbackReference = 0
   public sellerReference = ""
   public sellerReferenceId = 0
   public arraySeller = new Array()
-  public disabled = true;
   public loaded = false;
-  public minimoComra: any
 
-  public isCashbackProduct = false
-
+  // muestra u oculta alerta de cashback
+  public minimoCompra: any = 0
+  public isTotalCompra = false
 
   constructor(
     public alertController: AlertController,
@@ -39,35 +41,45 @@ export class CarPage implements OnInit {
     public nvCtrl: NavController,
   ) { }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void { }
 
   ionViewDidEnter() {
     this.setProductsIntoArray().finally(() => {
       this.loaded = true;
     })
 
+    // Llena el arreglo de cashbacks
     this.shopingService.getClientCashback(this.loginService.getUserCode()).then(() => {
       this.arrayCashback = this.shopingService.arrayDataCashback
     })
 
+    // Llena el arreglo de vendedores
     this.shopingService.getClientSeller(this.loginService.getUserCode()).then(() => {
       this.arraySeller = this.shopingService.arrayDataSeller
     })
 
-    this.shopingService.getMinimoCompra(this.loginService.validateSession()['bodcli_b']).then(() => {
-      this.minimoComra = parseFloat(localStorage.getItem("MinimoCompra"))
-    }).finally(() => {
-      this.getPriceTotalProducts()
-      this.getPriceProcess()
-    })
+    // Obtiene el minimo de compra por vendedor
+    this.getMinPurchase()
+
   }
 
+  // Retorna el listado de productos
   public getCarLocalStorage() {
     return JSON.parse(window.localStorage.getItem("productsCar"))
   }
 
+  // Obtiene el minimo de compra por vendedor
+  public getMinPurchase() {
+    this.shopingService.getMinimoCompra(this.loginService.validateSession()['bodcli_b']).then(
+      () => {
+        this.minimoCompra = this.convertNumberToDecimal(localStorage.getItem("MinimoCompra"))
+      }).finally(() => {
+        // Renderiza el Total y Subtotal de la orden
+        this.getPriceTotalProducts()
+      })
+  }
+
+  // Agregar productos a un array de productos
   public async setProductsIntoArray() {
     this.arrayProducts = []
 
@@ -78,18 +90,56 @@ export class CarPage implements OnInit {
     });
   }
 
+  // Valida si el producto tiene cashback
   public productWithCashback(productObject) {
     if (productObject.valor > "0") {
       productObject.isCashback = true
     }
   }
 
+  // Valida si el producto tiene descuento
   public productWithDiscount(productObject) {
     const descuentoFormated = parseInt(productObject.porcDescuento)
 
     if (descuentoFormated.toFixed(0) > "0") {
       productObject.isOffert = true
     }
+  }
+
+  // Obtiene el número actual de productos en el carrito
+  public currentProductsCarNumber() {
+    if (!this.getCarLocalStorage()) {
+      return 0
+    } else {
+      return this.getCarLocalStorage().length
+    }
+  }
+
+  // Valida si el producto es cashback o descuento
+  public storageCategoryProduct(object) {
+    const descuentoFormated = parseInt(object.porcDescuento)
+
+    if (object.valor > "0") {
+      localStorage.setItem("categoryProduct", "cashback")
+    } else if (descuentoFormated.toFixed(0) > "0") {
+      localStorage.setItem("categoryProduct", "descuento")
+    } else {
+      localStorage.removeItem("categoryProduct")
+    }
+  }
+
+  // Convierte cualquier numero a decimal
+  public convertNumberToDecimal(number: any): any {
+    const options = {
+      style: 'decimal',
+      minimumFractionDigits: 3,
+      maximumFractionDigits: 3,
+      useGrouping: true
+    };
+    const numberFormated = parseFloat(number).toLocaleString('en-US', options)
+
+
+    return numberFormated
   }
 
   public removeProductIntoCar(productId) {
@@ -101,8 +151,6 @@ export class CarPage implements OnInit {
 
         localStorage.setItem("productsCar", JSON.stringify(updatedCarProduct))
         this.arrayProducts = updatedCarProduct
-
-        this.totalProductPrice = 0
 
         this.getPriceTotalProductsRemove()
 
@@ -118,18 +166,13 @@ export class CarPage implements OnInit {
 
   }
 
-  public currentProductsCarNumber() {
-    if (!this.getCarLocalStorage()) {
-      return 0
-    } else {
-      return this.getCarLocalStorage().length
-    }
-  }
-
+  // Devuelve el Total y Subtotal de la orden con o sin cashback
   public getPriceTotalProducts() {
 
-    this.totalProductPrice = 0
+    // Inicializar siempre los totales en 0
+    this.subtotalProductPrice = 0
 
+    // Valida si existen productos en el carro
     if (this.getCarLocalStorage()) {
       this.getTotalAndSubtotal()
     } else {
@@ -138,15 +181,54 @@ export class CarPage implements OnInit {
 
   }
 
-  storageCategoryProduct(object) {
-    const descuentoFormated = parseInt(object.porcDescuento)
+  // Obtener el valor total del carrito sin subtotal ni descuento
+  public getTotalValueOfCar() {
+    let totalCarValue = 0;
 
-    if (object.valor > "0") {
-      localStorage.setItem("categoryProduct", "cashback")
-    } else if (descuentoFormated.toFixed(0) > "0") {
-      localStorage.setItem("categoryProduct", "descuento")
+    this.getCarLocalStorage().forEach(product => {
+
+      // Multiplica la cantidad de cada producto por su valor
+      let valueOfProductQuantity = this.convertNumberToDecimal(Number(product.price)) * product.quantityProduct
+
+      // Acumulamos la suma de cada valor en la variable totalCarValue
+      totalCarValue += valueOfProductQuantity;
+    })
+
+    const totalCarValueFormated = this.convertNumberToDecimal(totalCarValue)
+
+    return totalCarValueFormated
+  }
+
+  // Iguala el total y el subtotal de la orden
+  public getTotalAndSubtotal() {
+
+    // Total de la orden con cashback y normal
+    if (this.isCashbackApply) {
+      const totalOrderWithCashback = this.convertToFloat(this.getTotalValueOfCar()) - this.convertToFloat(this.totalCashback)
+      this.totalProductPriceProcess = this.convertNumberToDecimal(totalOrderWithCashback)
     } else {
-      localStorage.removeItem("categoryProduct")
+      this.totalProductPriceProcess = this.getTotalValueOfCar()
+    }
+
+    // Subtotal de la orden
+    this.subtotalProductPrice = this.getTotalValueOfCar()
+
+    this.validateMinValueOfOrder()
+
+    this.saveOrderDetailIntoLocalStorage()
+  }
+
+  // Convierte los números a floats Para hacer oPeraciones
+  public convertToFloat(str) {
+    const num = parseFloat(str.replace(',', ''));
+    return num
+  }
+
+  public validateMinValueOfOrder() {
+    if (this.convertToFloat(this.totalProductPriceProcess) > this.convertToFloat(this.minimoCompra)) {
+      this.isTotalCompra = true
+    } else {
+      this.isTotalCompra = false
     }
   }
 
@@ -158,49 +240,24 @@ export class CarPage implements OnInit {
         this.nvCtrl.navigateForward("/tabs/home").then(() => {
           document.querySelector(".js-ico-car").classList.add("test")
           document.querySelector(".js-search-header").classList.add("is-none-car")
-
           document.querySelector(".js-number-card-product-card").innerHTML = "1"
         })
       }
     }, 400)
   }
 
-  public getTotalAndSubtotal() {
-    this.subtotalProductPrice = 0
-
-    this.getCarLocalStorage().forEach(product => {
-      const productQuantity = parseFloat(product.price) * product.quantityProduct
-
-      this.subtotalProductPrice = this.subtotalProductPrice + productQuantity
-
-      if (this.isCashbackApply) {
-        this.totalProductPrice = this.subtotalProductPrice - this.totalCashback
-      } else {
-        this.totalProductPrice = this.subtotalProductPrice
-      }
-
-    });
-
-    this.subtotalProductPrice = this.subtotalProductPrice
-    this.getPriceProcess()
-
-    this.saveOrderDetailIntoLocalStorage()
-  }
-
+  // Seleccionar cashback
   public selectCashback(cashbackObject) {
-    let totalWithCashback = 0
-    this.subtotalProductPrice = this.getPriceProcess()
-    totalWithCashback = this.getPriceProcess() - cashbackObject.dinero
-    this.totalCashback = cashbackObject.dinero
-    this.totalProductPriceProcess = totalWithCashback
-    this.isCashbackApply = true
     document.querySelector(".js-car-dropdown").classList.remove("is-dropdown-show")
 
+    this.totalCashback = cashbackObject.dinero
+    this.isCashbackApply = true
     this.cashbackReference = cashbackObject.referencia
 
-    return this.totalProductPriceProcess
+    this.getTotalAndSubtotal()
   }
 
+  // Seleccionar vendedor
   public selectSeller(sellerObject) {
     this.isSellerApply = true
     document.querySelector(".js-car-dropdown-seller").classList.remove("is-dropdown-show")
@@ -209,74 +266,14 @@ export class CarPage implements OnInit {
     this.sellerReferenceId = sellerObject.codigoVen
   }
 
-  public getPriceProcess(): any {
-    this.totalProductPriceProcess = this.totalProductPrice
-
-    console.log("ESTEEE")
-
-    let num = this.totalProductPriceProcess;
-    let numFormatted = num.toString(); // "495.05"
-    let decimalIndex = numFormatted.indexOf("."); // 3
-    if (decimalIndex === -1) {
-      numFormatted += ".000"; // si no hay decimales, se agregan tres ceros al final
-    } else {
-      let decimals = numFormatted.substring(decimalIndex + 1); // "05"
-      if (decimals.length < 3) {
-        numFormatted += "0".repeat(3 - decimals.length); // si hay menos de tres decimales, se agregan los ceros faltantes
-      }
-    }
-
-    console.log("DOS INTNER")
-    console.log(this.totalProductPriceProcess)
-    console.log(this.totalProductPriceProcess.toString().length)
-
-    // if (this.totalProductPriceProcess.toString().length == 5) {
-    //   const este = parseFloat(numFormatted).toLocaleString('en-US') + "00"
-    //   console.log("ANTES")
-    //   console.log(este)
-    //   this.totalProductPriceProcess = este
-    //   console.log("desues 00")
-    //   console.log(this.totalProductPriceProcess)
-    // }
-    // else if (this.totalProductPriceProcess.toString().length == 7 || this.totalProductPriceProcess.toString().length == 6) {
-    //   const este = parseFloat(numFormatted).toLocaleString('en-US') + "0"
-    //   console.log("ANTES")
-    //   console.log(este)
-    //   this.totalProductPriceProcess = este
-    //   console.log("desues")
-    //   console.log(this.totalProductPriceProcess)
-    // } else {
-    //   console.log(parseFloat(numFormatted).toLocaleString('en-US'))
-    // }
-    
-    const options = {
-      style: 'decimal',
-      minimumFractionDigits: 3,
-      maximumFractionDigits: 3,
-      useGrouping: true
-    };
-
-    console.log("parseFloat(numFormatted).toLocaleString('en-US')")
-    console.log(parseFloat(numFormatted).toLocaleString('en-US', options))
-    this.totalProductPriceProcess = parseFloat(numFormatted).toLocaleString('en-US', options)
-
-    if (
-      this.totalProductPriceProcess
-      > this.minimoComra
-    ) {
-      this.isTotalCompra = true
-    } else {
-      this.isTotalCompra = false
-    }
-    return this.totalProductPriceProcess
-  }
-
+  // prepara la orden
   public saveOrderIntoLocalStorage() {
     this.shopingService.setArrayOfOrder(this.cashbackReference, this.sellerReferenceId)
     this.saveOrderDetailIntoLocalStorage()
     this.nvCtrl.navigateForward("/tabs/car-detail")
   }
 
+  // Guarda el detalle de la orden  en el storage
   public saveOrderDetailIntoLocalStorage() {
     const dataTemp = {
       "cashback": this.totalCashback,
